8296403bfb93cdd0093e3b669f27573ee50aad2f
code-udit <uditgunagi963@gmail.com>
2026-04-07 00:23:33 +0530

    Updation in code

--- services/api.js
+++ services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.BACKEND_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const rankResumes = async (files, jobDescription) => {
   const formData = new FormData();
